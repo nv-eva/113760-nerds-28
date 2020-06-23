fa695a43360ae18b0a22e58966cb85d4b6ad29ab
nv-eva <ds222591@rambler.ru>
2020-06-23 17:08:54 +0300

    Добавляем модальному окну скрипты

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/* FeedbackPopup */
+
+var feedbackLink = document.querySelector(".button-write");
+var feedbackPopup = document.querySelector(".modal-feedback");
+var feedbackClose = feedbackPopup.querySelector(".feedback-close");
+var feedbackForm = feedbackPopup.querySelector(".feedback-form");
+var feedbackName = feedbackPopup.querySelector(".feedback-form-name");
+var feedbackMail = feedbackPopup.querySelector(".feedback-form-email");
+var feedbackText = feedbackPopup.querySelector(".feedback-form-area");
+
+feedbackLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add("modal-show");
+  feedbackName.focus();
+});
+
+feedbackClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove("modal-show");
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackName.value) {
+    evt.preventDefault();
+    feedbackName.classList.add("feedback-form-error");
+  } else {
+    feedbackName.classList.remove("feedback-form-error");
+  }
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackMail.value) {
+    evt.preventDefault();
+    feedbackMail.classList.add("feedback-form-error");
+  } else {
+    feedbackMail.classList.remove("feedback-form-error");
+  }
+});
+
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!feedbackText.value) {
+    evt.preventDefault();
+    feedbackText.classList.add("feedback-form-error");
+  } else {
+    feedbackText.classList.remove("feedback-form-error");
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (feedbackPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      feedbackPopup.classList.remove("modal-show");
+    }
+  }
+});
